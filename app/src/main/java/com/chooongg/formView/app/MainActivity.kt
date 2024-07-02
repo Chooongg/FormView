12b6479478d8f5f4fbc5d6afb5b8e78e2899ae19
Chooongg <chooongg@outlook.com>
2024-07-02 16:23:50 +0800

    Initial commit

--- app/src/main/java/com/chooongg/formView/app/MainActivity.kt
+++ app/src/main/java/com/chooongg/formView/app/MainActivity.kt
@@ -1,17 +1,52 @@
 package com.chooongg.formView.app
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.chooongg.formView.app.databinding.ActivityMainBinding
+import com.chooongg.formView.app.fragment.AboutFragment
+import com.chooongg.formView.app.fragment.HomeFragment
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationBarView.OnItemSelectedListener {
 
     private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
+    private val fragments = arrayListOf(
+        HomeFragment(), AboutFragment()
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
+        binding.viewPager.isUserInputEnabled = false
+        binding.viewPager.adapter = Adapter(this, fragments)
+        with(binding.navigationView as NavigationBarView) {
+            onFragmentSelected(selectedItemId)
+            setOnItemSelectedListener(this@MainActivity)
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return onFragmentSelected(item.itemId)
+    }
+
+    private fun onFragmentSelected(itemId: Int): Boolean {
+        when (itemId) {
+            R.id.nav_home -> binding.viewPager.setCurrentItem(0, false)
+            R.id.nav_about -> binding.viewPager.setCurrentItem(1, false)
+            else -> return false
+        }
+        return true
+    }
+
+    class Adapter(activity: AppCompatActivity, private val fragments: List<Fragment>) :
+        FragmentStateAdapter(activity) {
+        override fun getItemCount(): Int = fragments.size
+        override fun createFragment(position: Int): Fragment = fragments[position]
     }
 }
---
+++ app/src/main/java/com/chooongg/formView/app/fragment/AboutFragment.kt
@@ -0,0 +1,28 @@
+package com.chooongg.formView.app.fragment
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.chooongg.formView.app.databinding.FragmentAboutBinding
+
+class AboutFragment : Fragment() {
+
+    private lateinit var binding: FragmentAboutBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAboutBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.webView.background = null
+        binding.webView.settings.javaScriptEnabled = true
+        binding.webView.loadUrl("https://github.com/Chooongg/FormView2")
+    }
+}
