9ae90dc7097380d61f62490defbc82f4733ddef5
Chooongg <chooongg@outlook.com>
2024-07-11 17:13:33 +0800

    Initial commit

--- formView/src/main/java/com/chooongg/formView/FormItemKtx.kt
+++
@@ -1,8 +0,0 @@
-package com.chooongg.formView
-
-import com.chooongg.formView.data.IFormGroupData
-import com.chooongg.formView.item.FormText
-
-fun IFormGroupData.text(
-    name: Any?, field: String? = null, content: Any? = null, block: (FormText.() -> Unit)? = null
-) = addItem(FormText(name, field, content).apply { block?.invoke(this) })
--- formView/src/main/java/com/chooongg/formView/data/IFormGroupData.kt
+++ formView/src/main/java/com/chooongg/formView/data/IFormGroupData.kt
@@ -1,6 +1,7 @@
 package com.chooongg.formView.data
 
 import com.chooongg.formView.item.BaseForm
+import com.chooongg.formView.item.FormText
 
 interface IFormGroupData {
 
@@ -8,4 +9,10 @@ interface IFormGroupData {
 
     fun addItem(item: BaseForm<*>) = getItems().add(item)
 
+    fun text(
+        name: Any?,
+        field: String? = null,
+        content: Any? = null,
+        block: (FormText.() -> Unit)? = null
+    ) = addItem(FormText(name, field, content).apply { block?.invoke(this) })
 }
