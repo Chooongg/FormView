8e48cc00e1888201cc75591152765dfb4afe95b8
Chooongg <chooongg@outlook.com>
2024-07-12 17:11:44 +0800

    Initial commit

--- formView/src/main/java/com/google/android/flexbox/FlexInternalFunction.kt
+++
@@ -1,7 +0,0 @@
-package com.google.android.flexbox
-
-object FlexInternalFunction {
-    fun getPositionToFlexLineIndex(flexboxLayoutManager: FlexboxLayoutManager, position: Int): Int {
-        return flexboxLayoutManager.getPositionToFlexLineIndex(position)
-    }
-}
